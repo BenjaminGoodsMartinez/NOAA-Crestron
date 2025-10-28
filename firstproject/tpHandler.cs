6ad360c36ca916a60a4acd6fde16c8193bea0267
BenjaminMartinez <benjamin.martinez119@gmail.com>
2025-10-28 08:08:59 +0000

    first hanges

---
+++ firstproject/tpHandler.cs
@@ -0,0 +1,15 @@
+﻿using Crestron.SimplSharpPro;
+
+namespace firstproject
+{
+    public class tpHandler
+    {
+        private void tpHandlerDigital(SigEventArgs args)
+        {
+            if (args.Sig.BoolValue)
+            {
+                
+            }
+        }
+    }
+}
